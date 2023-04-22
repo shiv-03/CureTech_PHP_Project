1797e88b7478a972c862fec647d44a4090741c95
Shivam Singh <shivamsingh856391@gmail.com>
2023-04-23 00:27:44 +0530

    PHP Project

---
+++ header.php
@@ -0,0 +1,45 @@
+<!-- Navigation Bar -->
+
+<nav class="navbar navbar-expand-lg " style="background-color: light grey ;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <h1 style="font-family:Times New Roman"><B>Cure Tech</B></h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lab Tests</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                            Ayush
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Ayurvedic</a></li>
+                            <li><a class="dropdown-item" href="#">Unani</a></li>
+                            <li><a class="dropdown-item" href="#">Homeopathy</a></li>
+                            <li><a class="dropdown-item" href="#">Siddha</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Health Blogs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About Us</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+
+            </div>
+        </div>
+    </nav>
