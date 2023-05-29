e32e299491ed76714227b1619cdf3588e58f4317
Shivam Singh <shivamsingh856391@gmail.com>
2023-05-30 02:37:39 +0530

    updated the css

--- HealthBlogs.php
+++ HealthBlogs.php
@@ -14,18 +14,67 @@ $username  = "root";
 $password = "";
 $database = "Concern";
 
+
 $conn = mysqli_connect($servername, $username, $password, $database);
+
 
 if(!$conn){
     die ("your connection doesnt established ".mysqli_connect_error());
 
 } 
-// else{
-//     echo "your connection has been established";
-// }
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+$Name = $_POST['Name'];
+$Blog = $_POST['Article'];
+
+$sql = "INSERT INTO `healthblogs` (`Name`, `Article`) VALUES ('$Name', '$Blog');";
+
+$result = mysqli_query($conn , $sql);
+
+if ($result)
+{
+    $insert = true; 
+}
+else{
+    echo "Something went wrong ".mysqli_connect_error();
+}
+}
 
 ?>
 
+<div class="banner_w3lspvt position-relative">
+        <div class="container">
+            <div class="d-md-flex">
+                <div class="w3ls_banner_txt">
+                    <h3 class="w3ls_pvt-title">Are blogs <br><span>Healthy?</span></h3>
+                    <p class="text-sty-banner"><b>Definitely Yes !</b> because sharing your thoughts can help someone else life .Health blogs offer readers a platform to source answers, knowledge, opinion, support and/or guidance while managing their experience. They have brought together users of similar interest closer to finding whatever health information they seek. So, why wait start writing your healthy thoughts here and get accomplished by the Team CURE TECH .</p>
+                       </div>
+                <div class="banner-img">
+                    <img src="ProjectPhotos/1.jpg" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+
+  <!-- form section -->
+<div class="container">
+  
+<form action="/crud/HealthBlogs.php" method="post">
+  <div class="form-group">
+    <h2 class="">Healthy thoughts are Welcome!!</h2>
+    <label for="exampleFormControlInput1" class="my-3">Your Healthy Name</label>
+    <input type="Name" class="Name form-control " id="exampleFormControlInput1" placeholder="Enter your name here...">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1" class="my-3">Health Pad</label>
+    <textarea class="Blog form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter your article here...."> </textarea>
+  </div>
+  <button type="submit" class="btn btn-primary my-3">Submit</button>
+</form>
+
+</div>
 
 
 <?php include 'footer.php' ; ?>
--- LabTests.php
+++ LabTests.php
@@ -1,11 +1,128 @@
 <link rel="stylesheet" href="footer.css">
+<link rel="stylesheet" href="LabTest.css">
+<link href="htttps://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<style>
+  .slot-box{
+    padding: 4px 4px 4px 4px;
+    margin: 4px 4px 4px 4px;
+    display:flex;
+    justify-content:space-around;
+    border: 1px solid grey;
+    border-radius: 5px;
+    
+  }
+  .slot-box:hover{
+    animation: slot 5s;
+  }
+  .booking-slots{
+    paddding: 10px 10px ;
+    margin: 5px 5px 5px 5px;
+  }
+  @keyframes slot{
+    50% {background-color: #aaa;}
+  }
+</style>
+
 
 <?php include 'header.php'; ?>
 
+<div class=" container">
+<table class="#myTable">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+         
+<!-- <div class = "booking-slots py-3 my-4">
+  <!-- <h1>Book your slots here....</h1> -->
 
 
+<!-- <div class="slot-box py-3 my-4">
+  <div class="type"><h3>Covid</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Widal Test</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>HbA1c(Glycated Haemoglobin)</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Tumor marker tests</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Urine Cytology</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Aminocentesis</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Liver function test</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Kidney function test</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Lipid Profile</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Sputum Cytology</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div>
+<div class="slot-box py-3 my-4">
+  
+  <div class="type"><h3>Ultrasonography</h3></div>
+  <div class="slot-btn"><button type="button" class="btn btn-primary">Book Slot</button></div>
+</div> --> -->
 
-
+</div>
 <?php include 'footer.php' ; ?>
+<script>
+  let table = new DataTable('#myTable');
+</script>
+<script href="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
